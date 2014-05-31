fb5c7485065a160b5567c6e536b1e2597ec0bcf9
Michael Phan-Ba <michael@mikepb.com>
2014-05-31 15:31:03 -0700

    Fix handling of bodiless requests

--- test/socket.test.js
+++ test/socket.test.js
@@ -9,9 +9,13 @@ describe('Protocol Buffer Socket', function () {
   before(function () {
     this.socket = new MockSocket({
       returns: [
+        new Buffer('AAAACRAKBHRlc3QQAQ==', 'base64'),
         new Buffer('AAAAHQgKDnJpYWtAMTI3LjAuMC4xEgoyLjAuMGJldGEx', 'base64')
       ],
       asserts: [
+        function (chunk) {
+          assert.equal(chunk.toString('base64'), 'AAAAAQ8=');
+        },
         function (chunk) {
           assert.equal(chunk.toString('base64'), 'AAAAAQc=');
         }
@@ -27,7 +31,18 @@ describe('Protocol Buffer Socket', function () {
   });
 
   it('should use pipeline', function (done) {
-    this.subject.write('RpbGetServerInfoReq');
+    this.subject.once('data', function (data) {
+      assert.equal(data._type, 'RpbListBucketsResp');
+      assert.deepEqual(data, {
+        buckets: ['test'],
+        done: true
+      });
+      done();
+    }.bind(this));
+    this.subject.write({}, 'RpbListBucketsReq');
+  });
+
+  it('should use pipeline with no body', function (done) {
     this.subject.once('data', function (data) {
       assert.equal(data._type, 'RpbGetServerInfoResp');
       assert.deepEqual(data, {
@@ -36,6 +51,7 @@ describe('Protocol Buffer Socket', function () {
       });
       done();
     }.bind(this));
+    this.subject.write('RpbGetServerInfoReq');
   });
 
   it('should end', function () {
